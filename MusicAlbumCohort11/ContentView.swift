7843e80bcc4c754d23190a4ce7e93871bd120f43
brittanygrigsby <brittanysugrigsby@gmail.com>
2026-08-24 20:51:03 -0700

    Initial Commit

---
+++ MusicAlbumCohort11/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MusicAlbumCohort11
+//
+//  Created by Brittany Grigsby on 8/24/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MusicAlbumCohort11/MusicAlbumCohort11App.swift
@@ -0,0 +1,17 @@
+//
+//  MusicAlbumCohort11App.swift
+//  MusicAlbumCohort11
+//
+//  Created by Brittany Grigsby on 8/24/26.
+//
+
+import SwiftUI
+
+@main
+struct MusicAlbumCohort11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
